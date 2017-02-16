d354a650997b8d52b0ed7ba2d931acbc21e0d438
Farhan1989 <farhanultimate100@gmail.com>
2017-02-16 17:59:55 +0000

    Master data for Role table

---
+++ src/main/resources/DbScripts/MasterData.sql
@@ -0,0 +1,3 @@
+INSERT INTO `db_tapserve`.`role` (`ID`, `NAME`, `DESCRIPTION`) VALUES ('1', 'USER', 'Normal User. This user will book appointment with service providers');
+INSERT INTO `db_tapserve`.`role` (`ID`, `NAME`, `DESCRIPTION`) VALUES ('2', 'SERVICE_PROVIDER', 'This user will provide services to normal user');
+INSERT INTO `db_tapserve`.`role` (`ID`, `NAME`, `DESCRIPTION`) VALUES ('3', 'ADMIN', 'admin user for the portal');
