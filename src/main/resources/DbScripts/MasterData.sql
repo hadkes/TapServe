ae7492f0c78115c4c1944bc31081d1462a67c40a
sagarsachdeva <sachdevs@tcd.ie>
2017-02-17 17:23:18 +0000

    Added Insert statements to service table

--- src/main/resources/DbScripts/MasterData.sql
+++ src/main/resources/DbScripts/MasterData.sql
@@ -1,3 +1,9 @@
 INSERT INTO `db_tapserve`.`role` (`ID`, `NAME`, `DESCRIPTION`) VALUES ('1', 'USER', 'Normal User. This user will book appointment with service providers');
 INSERT INTO `db_tapserve`.`role` (`ID`, `NAME`, `DESCRIPTION`) VALUES ('2', 'SERVICE_PROVIDER', 'This user will provide services to normal user');
 INSERT INTO `db_tapserve`.`role` (`ID`, `NAME`, `DESCRIPTION`) VALUES ('3', 'ADMIN', 'admin user for the portal');
+insert into `db_tapserve`.`SERVICE` values("1","electrician","electrician");
+insert into `db_tapserve`.`SERVICE` values("2","carpenter","carpenter");
+insert into `db_tapserve`.`SERVICE` values("3","plumber","plumber");
+insert into `db_tapserve`.`SERVICE` values("4","eventPlanner","eventPlanner");
+insert into `db_tapserve`.`SERVICE` values("5","photographer","photographer");
+insert into `db_tapserve`.`SERVICE` values("6","yogaInstructor","yogaInstructor");
