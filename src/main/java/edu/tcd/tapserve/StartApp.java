01cc0d6d32cd0ccd6072eab1aaa13ccf66fbfefa
Sachin Hadke <hadkes@tcd.ie>
2017-02-19 14:03:38 +0000

    added logging in StartApp and Controller

--- src/main/java/edu/tcd/tapserve/StartApp.java
+++ src/main/java/edu/tcd/tapserve/StartApp.java
@@ -1,11 +1,16 @@
 package edu.tcd.tapserve;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StartApp {
+	private static final Logger logger = LoggerFactory.getLogger(StartApp.class);
+
 	public static void main(String[] args) {
+		logger.debug("Staring spring boot configuration.");
 		SpringApplication.run(StartApp.class, args);
 	}
 }
--- src/main/java/edu/tcd/tapserve/controller/RegistrationController.java
+++ src/main/java/edu/tcd/tapserve/controller/RegistrationController.java
@@ -2,6 +2,8 @@ package edu.tcd.tapserve.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.tcd.tapserve.StartApp;
 import edu.tcd.tapserve.bean.Administrator;
 import edu.tcd.tapserve.bean.Credentials;
 import edu.tcd.tapserve.bean.Service;
@@ -19,13 +22,15 @@ import edu.tcd.tapserve.service.RegistrationService;
 
 @RestController
 public class RegistrationController {
-
+	private static final Logger logger = LoggerFactory.getLogger(StartApp.class);
+	
 	@Autowired
 	RegistrationService registrationService;
 
 	@RequestMapping(method = RequestMethod.POST, value = "/register")
 	@CrossOrigin(origins = "http://localhost:8080")
 	public String registerActor(@RequestBody Credentials credentials) {
+		logger.debug("Registering "+credentials.getActorId()+" with name "+credentials.getUsername());
 		return registrationService.addCredentials(credentials);
 	}
 
