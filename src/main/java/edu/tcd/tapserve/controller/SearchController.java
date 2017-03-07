54da547f668672514433a556d43e9034c40f638b
ahmadfa <ahmadfa@tcs.ie>
2017-03-07 20:00:51 +0000

    changes for cross domain request

--- src/main/java/edu/tcd/tapserve/controller/SearchController.java
+++ src/main/java/edu/tcd/tapserve/controller/SearchController.java
@@ -3,6 +3,7 @@ package edu.tcd.tapserve.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,7 @@ public class SearchController {
 	SearchService searchService;
 
 	@RequestMapping(method = RequestMethod.GET, value = "/findServiceProviders/{userId}/{regionId}/{serviceId}")
+	@CrossOrigin(origins = "http://localhost:8080")
 	public List<CustomServiceProvider> getSearchResult(@PathVariable String userId, @PathVariable String regionId,
 			@PathVariable String serviceId) {
 
